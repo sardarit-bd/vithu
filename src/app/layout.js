364d90fce62a4cfaa301dec0cfd91e31abbdc965
Sardar IT <sardaritbd.official@gmail.com>
2026-02-13 10:20:26 +0600

    analytics code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -72,6 +72,20 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} ${Comic.variable} ${Aptos.variable} antialiased`}
       >
         {children}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17945744221"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17945744221');
+          `}
+        </Script>
       </body>
     </html>
   );
